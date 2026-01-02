8473918f5a031539eade8f785386a91457071575
Your Name <psinthorn@gmail.com>
2026-01-02 09:15:20 +0700

    Major cleanup: Remove duplicates and organize files
    - Deleted 70 duplicate PHP files from root (kept in iacc/)
    - Deleted duplicate MPDF folders (MPDF/, MPDF57-7/) - keeping iacc/MPDF
    - Moved 83 markdown files to docs/
    - Moved SQL backups to backups/
    - Deleted 29 .backup files from iacc/
    - Deleted php-source/ and debug files
    
    Size reduced from 482MB to 418MB (~64MB saved)

--- MPDF57-7/classes/.!7703!svg.php
+++
@@ -1,7 +0,0 @@
-<?php
-//	svg class modified for mPDF version 5.7.4 by Ian Back: based on -
-//	svg2pdf fpdf class
-//	sylvain briand (syb@godisaduck.com), modified by rick trevino (rtrevino1@yahoo.com)
-//	http://www.godisaduck.com/svg2pdf_with_fpdf
-//	http://rhodopsin.blogspot.com
-//
