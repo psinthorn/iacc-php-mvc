39e250ff8a3274e7d826770cc9c94e0183f804a2
Your Name <psinthorn@gmail.com>
2026-01-02 13:48:45 +0700

    Security: Secure remoteuser.php and fetadr.php
    - remoteuser.php: Use sql_int() for company ID
    - fetadr.php: Use sql_int() for address ID, e() for output

--- fetadr.php
+++ fetadr.php
@@ -2,9 +2,11 @@
 session_start();
 require_once("inc/sys.configs.php");
 require_once("inc/class.dbconn.php");
+require_once("inc/security.php");
 $users=new DbConn($config);
 $users->checkSecurity();
-$data=mysql_fetch_array(mysql_query("select adr_tax,city_tax,district_tax,province_tax,zip_tax from company_addr where id='".$_REQUEST[id]."'"));
-				 echo   $data[adr_tax].";".$data[city_tax].";".$data[district_tax].";".$data[province_tax].";".$data[zip_tax];
+$addr_id = sql_int($_REQUEST['id']);
+$data=mysql_fetch_array(mysql_query("select adr_tax,city_tax,district_tax,province_tax,zip_tax from company_addr where id='".$addr_id."'"));
+echo e($data['adr_tax']).";".e($data['city_tax']).";".e($data['district_tax']).";".e($data['province_tax']).";".e($data['zip_tax']);
 
 ?>
